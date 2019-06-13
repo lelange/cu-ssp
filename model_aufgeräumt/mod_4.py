86e9b3491c9a92ebf94289413b70b6e25d56a5ea
annalena <annalena.alnge>
2019-06-13 10:36:11 +0200

    add utils to orderd models

--- model_aufgeräumt/mod_4.py
+++ model_aufgeräumt/mod_4.py
@@ -16,6 +16,8 @@ from keras.callbacks import TensorBoard, LearningRateScheduler, ModelCheckpoint,
 from datetime import datetime
 import os, pickle
 
+from utils import *
+
 '''
 various helper functions
 '''
@@ -30,74 +32,6 @@ def encode_FOFE(onehot, alpha, maxlen):
         enc[maxlen-i-1, 22:] = enc[maxlen-i, 22:] * alpha + onehot[maxlen-i-1]
     return enc
 
-# The custom accuracy metric used for this task
-def accuracy(y_true, y_predicted):
-    y = tf.argmax(y_true, axis =- 1)
-    y_ = tf.argmax(y_predicted, axis =- 1)
-    mask = tf.greater(y, 0)
-    return K.cast(K.equal(tf.boolean_mask(y, mask), tf.boolean_mask(y_, mask)), K.floatx())
-
-# Convert probabilities to secondary structure
-def to_seq(y):
-    seqs=[]
-    for i in range(len(y)):
-        seq_i=''
-        for j in range(len(y[i])):
-            seq_i += q8_list[np.argmax(y[i][j])]
-        seqs.append(seq_i)
-    return seqs
-
-def onehot_to_seq(oh_seq, index):
-    s = ''
-    for o in oh_seq:
-        i = np.argmax(o)
-        if i != 0:
-            s += index[i]
-        else:
-            break
-    return s
-
-counter = 0
-# prints the results
-def print_results(x, y_, revsere_decoder_index, counter,test_df, write_df=False, print_pred=False):
-    if write_df:
-        Ans['id'][counter] = test_df['id'][counter]
-        Ans['expected'][counter] = str(onehot_to_seq(y_, revsere_decoder_index).upper())
-    if print_pred:
-        print("prediction: " + str(onehot_to_seq(y_, revsere_decoder_index).upper()))
-
-#
-def seq2ngrams(seqs, n = 1):
-    return np.array([[seq[i : i + n] for i in range(len(seq))] for seq in seqs])
-
-
-def load_augmented_data(npy_path, max_len):
-    data = np.load(npy_path)
-    residue_list = ['A', 'C', 'E', 'D', 'G', 'F', 'I', 'H', 'K', 'M', 'L', 'N', 'Q', 'P', 'S', 'R', 'T', 'W', 'V', 'Y', 'X','NoSeq']
-    q8_list = ['L', 'B', 'E', 'G', 'I', 'H', 'S', 'T','NoSeq']
-
-    data_reshape = data.reshape(data.shape[0], 700, -1)
-    residue_onehot = data_reshape[:,:,0:22]
-    residue_q8_onehot = data_reshape[:,:,22:31]
-    profile = data_reshape[:,:,35:57]
-    zero_arr = np.zeros((profile.shape[0], max_len - profile.shape[1], profile.shape[2]))
-    profile_padded = np.concatenate([profile, zero_arr], axis=1)
-
-    residue_array = np.array(residue_list)[residue_onehot.argmax(2)]
-    q8_array = np.array(q8_list)[residue_q8_onehot.argmax(2)]
-    residue_str_list = []
-    q8_str_list = []
-    for vec in residue_array:
-        x = ''.join(vec[vec != 'NoSeq'])
-        residue_str_list.append(x)
-    for vec in q8_array:
-        x = ''.join(vec[vec != 'NoSeq'])
-        q8_str_list.append(x)
-
-    id_list = np.arange(1, len(residue_array) + 1)
-    len_list = np.array([len(x) for x in residue_str_list])
-    train_df = pd.DataFrame({'id': id_list, 'len': len_list, 'input': residue_str_list, 'expected': q8_str_list})
-    return train_df, profile_padded
 
 '''
 Getting Data
