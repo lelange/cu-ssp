494255ef28334f99c7b9482094aa6155d998abeb
annalena <annalena.alnge>
2019-06-19 14:31:53 +0200

    mod 6 more epochs

--- model_aufgeräumt/mod_6.py
+++ model_aufgeräumt/mod_6.py
@@ -93,13 +93,13 @@ model.summary()
 
 ### monitor = 'val_weighted_accuracy'
 earlyStopping = EarlyStopping(monitor='val_accuracy', patience=10, verbose=1, mode='max')
-
+reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=8, min_lr=0.0005, verbose=1)
 load_file = "./model/mod_6-CB513-"+datetime.now().strftime("%Y_%m_%d-%H_%M")+".h5"
 
 checkpointer = ModelCheckpoint(filepath=load_file, verbose=1, save_best_only=True, mode='max')
 
 history=model.fit([X_train,X_aug_train], y_train, validation_data=([X_val, X_aug_val], y_val),
-        epochs=70, batch_size=128, callbacks=[checkpointer, earlyStopping], verbose=2, shuffle=True)
+        epochs=70, batch_size=128, callbacks=[checkpointer, earlyStopping, reduce_lr], verbose=2, shuffle=True)
 
 
 model.load_weights(load_file)
