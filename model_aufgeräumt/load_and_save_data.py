d65ae2b882a9b42d743b57b32be797656272048b
annalena <annalena.alnge>
2019-06-28 11:38:47 +0200

    debugging load data

--- model_aufgeräumt/load_and_save_data.py
+++ model_aufgeräumt/load_and_save_data.py
@@ -43,16 +43,17 @@ def reshape_and_pad(list):
     return data
 
 def normalize(data):
-    mean = np.mean(data)
-    std = np.std(data)
-    data = (data-mean)/std
-    return data
-
-def standardize(data):
     min = np.min(data)
     max = np.max(data)
-    data = (data-min)(max-min)
-    return data
+    data_ = (data-min)/(max-min)
+    return data_
+
+def standardize(data):
+    mean = np.mean(data)
+    std = np.std(data)
+    data_ = (data - mean) / std
+    return data_
+
 print("reshape and pad profiles...")
 train_pssm = reshape_and_pad(train_pssm_list)
 train_hmm = reshape_and_pad(train_hmm_list)
@@ -61,6 +62,7 @@ test_hmm = reshape_and_pad(test_hmm_list)
 
 train_profiles = np.concatenate((train_pssm, train_hmm),axis=2)
 test_profiles = np.concatenate((test_pssm, test_hmm),axis=2)
+
 print("normalize profiles...")
 train_pssm_norm = normalize(train_pssm)
 train_hmm_norm = normalize(train_hmm)
@@ -69,6 +71,7 @@ test_hmm_norm = normalize(test_hmm)
 
 train_profiles_norm = np.concatenate((train_pssm_norm, train_hmm_norm),axis=2)
 test_profiles_norm = np.concatenate((test_pssm_norm, test_hmm_norm),axis=2)
+
 print("standardize profiles ...")
 train_pssm_stan = standardize(train_pssm)
 train_hmm_stan = standardize(train_hmm)
@@ -77,6 +80,7 @@ test_hmm_stan = standardize(test_hmm)
 
 train_profiles_stan = np.concatenate((train_pssm_stan, train_hmm_stan),axis=2)
 test_profiles_stan = np.concatenate((test_pssm_stan, test_hmm_stan),axis=2)
+
 print("save data...")
 np.save('../data/train_q8.npy', train_dssp_q8)
 np.save('../data/test_q8.npy', test_dssp_q8)
