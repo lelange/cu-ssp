a3364a03c0fdd411f0d511dc3a2a92c857954d82
annalena <annalena.alnge>
2019-06-28 11:29:11 +0200

    debugging load data

--- model_aufgeräumt/load_and_save_data.py
+++ model_aufgeräumt/load_and_save_data.py
@@ -9,9 +9,12 @@ cullpdb_df = pd.DataFrame(cullpdb)
 data13_df = pd.DataFrame(data13)
 
 #load train, test, pssm-profiles and hmm-profiles which are not longer than maxlen_seq
+print('load data...')
+train_dssp = cullpdb_df[['dssp']][cullpdb_df['seq'].apply(len)<=maxlen_seq].values
+train_pssm_list, train_hmm_list= cullpdb_df[['pssm', 'hhm']][cullpdb_df['seq'].apply(len)<=maxlen_seq].values
 
-train_dssp, train_pssm_list, train_hmm_list= cullpdb_df[['dssp', 'pssm', 'hhm']][cullpdb_df['seq'].apply(len)<=maxlen_seq].values
-test_dssp, test_pssm_list, test_hmm_list = data13_df[['dssp', 'pssm', 'hhm']][data13_df['seq'].apply(len)<=maxlen_seq].values
+test_dssp = data13_df[['dssp']][data13_df['seq'].apply(len)<=maxlen_seq].values
+test_pssm_list, test_hmm_list = data13_df[['pssm', 'hhm']][data13_df['seq'].apply(len)<=maxlen_seq].values
 
 def make_q8(dssp):
     q8_beta = []
@@ -21,7 +24,7 @@ def make_q8(dssp):
     for item in q8_beta:
         q8.append(item.replace('_', 'L'))
     return q8
-
+print("change q8 sequences... ")
 train_dssp_q8 = make_q8(train_dssp)
 test_dssp_q8 = make_q8(test_dssp)
 
@@ -46,7 +49,7 @@ def standardize(data):
     max = np.max(data)
     data = (data-min)(max-min)
     return data
-
+print("reshape and pad profiles...")
 train_pssm = reshape_and_pad(train_pssm_list)
 train_hmm = reshape_and_pad(train_hmm_list)
 test_pssm = reshape_and_pad(test_pssm_list)
@@ -54,7 +57,7 @@ test_hmm = reshape_and_pad(test_hmm_list)
 
 train_profiles = np.concatenate((train_pssm, train_hmm),axis=2)
 test_profiles = np.concatenate((test_pssm, test_hmm),axis=2)
-
+print("normalize profiles...")
 train_pssm_norm = normalize(train_pssm)
 train_hmm_norm = normalize(train_hmm)
 test_pssm_norm = normalize(test_pssm)
@@ -62,7 +65,7 @@ test_hmm_norm = normalize(test_hmm)
 
 train_profiles_norm = np.concatenate((train_pssm_norm, train_hmm_norm),axis=2)
 test_profiles_norm = np.concatenate((test_pssm_norm, test_hmm_norm),axis=2)
-
+print("standardize profiles ...")
 train_pssm_stan = standardize(train_pssm)
 train_hmm_stan = standardize(train_hmm)
 test_pssm_stan = standardize(test_pssm)
@@ -70,7 +73,7 @@ test_hmm_stan = standardize(test_hmm)
 
 train_profiles_stan = np.concatenate((train_pssm_stan, train_hmm_stan),axis=2)
 test_profiles_stan = np.concatenate((test_pssm_stan, test_hmm_stan),axis=2)
-
+print("save data...")
 np.save('../data/train_q8.npy', train_dssp_q8)
 np.save('../data/test_q8.npy', test_dssp_q8)
 np.save('../data/train_profiles.npy', train_profiles)
