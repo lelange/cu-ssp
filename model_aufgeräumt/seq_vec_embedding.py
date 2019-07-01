5fa4c83e1a5bcfd389f24dd3cf6f0b1f214b4df8
annalena <annalena.alnge>
2019-07-01 14:40:50 +0200

    change seqvec

--- model_aufgeräumt/seq_vec_embedding.py
+++ model_aufgeräumt/seq_vec_embedding.py
@@ -6,17 +6,20 @@ model_dir = Path('../../seqVec')
 weights = model_dir / 'weights.hdf5'
 options = model_dir / 'options.json'
 seqvec  = ElmoEmbedder(options,weights,cuda_device=0) # cuda_device=-1 for CPU
+#inputs: primary structure
+train_input = np.load('../data/train_input.npy')
+test_input = np.load('../data/test_input.npy')
+
 #Get embedding for amino acid sequence:
-seq = 'SEQWENCE' # your amino acid sequence
+seq = train_input[0]
+print(seq.shape)
 embedding = seqvec.embed_sentence( list(seq) ) # List-of-Lists with shape [3,L,1024]
 
 #Get 1024-dimensional embedding for per-residue predictions:
-residue_embd = torch.tensor(embedding).sum(dim=0) # Tensor with shape [L,1024]
+#residue_embd = torch.tensor(embedding).sum(dim=0) # Tensor with shape [L,1024]
 #Get 1024-dimensional embedding for per-protein predictions:
 protein_embd = torch.tensor(embedding).sum(dim=0).mean(dim=0) # Vector with shape [1024]
-
-test = residue_embd.cpu().detach().numpy()
+test = protein_embd.cpu().detach().numpy()
 print(embedding.shape)
-print(residue_embd.shape)
 print(protein_embd.shape)
 print(test.shape)
