88b6fe50cb86fcda4b2877268a0441a637ccf03c
annalena <annalena.alnge>
2019-07-01 15:13:26 +0200

    change seqvec

--- model_aufgeräumt/seq_vec_embedding.py
+++ model_aufgeräumt/seq_vec_embedding.py
@@ -7,7 +7,7 @@ import argparse
 import time
 import numpy as np
 
-start_time = time.time()
+
 
 model_dir = Path('../../seqVec')
 weights = model_dir / 'weights.hdf5'
@@ -17,29 +17,39 @@ seqvec  = ElmoEmbedder(options,weights,cuda_device=0) # cuda_device=-1 for CPU
 train_input = np.load('../data/train_input.npy')
 test_input = np.load('../data/test_input.npy')
 
-#Get embedding for amino acid sequence:
-train_input_embedding = []
-times = []
-for i,seq in enumerate(train_input):
-    print('\n \n----------------------')
-    print('----------------------')
-    print(i, '/', len(train_input))
-    print('----------------------')
-    embedding = seqvec.embed_sentence( list(seq) ) # List-of-Lists with shape [3,L,1024]
+def calculate_and_save_embedding(input):
+    # Get embedding for amino acid sequence:
+    input_embedding = []
+    times = []
+    for i, seq in enumerate(input):
+        print('\n \n----------------------')
+        print('----------------------')
+        print('Sequence ', (i + 1), '/', len(input))
+        print('----------------------')
+        embedding = seqvec.embed_sentence(list(seq))  # List-of-Lists with shape [3,L,1024]
 
-    #Get 1024-dimensional embedding for per-residue predictions:
-    #residue_embd = torch.tensor(embedding).sum(dim=0) # Tensor with shape [L,1024]
-    #Get 1024-dimensional embedding for per-protein predictions:
-    protein_embd = torch.tensor(embedding).sum(dim=0).mean(dim=0) # Vector with shape [1024]
-    protein_embd_np = protein_embd.cpu().detach().numpy()
-    train_input_embedding.append(protein_embd_np)
-    time = time.time()-start_time
-    times.append(time)
-    print("For {} residues {:.0f}s needed.".format(len(train_input), time))
+        # Get 1024-dimensional embedding for per-residue predictions:
+        # residue_embd = torch.tensor(embedding).sum(dim=0) # Tensor with shape [L,1024]
+        # Get 1024-dimensional embedding for per-protein predictions:
+        protein_embd = torch.tensor(embedding).sum(dim=0).mean(dim=0)  # Vector with shape [1024]
+        protein_embd_np = protein_embd.cpu().detach().numpy()
+        input_embedding.append(protein_embd_np)
+        t = time.time() - start_time
+        times.append(t)
+        print("For {} residues {:.0f}s needed.".format(len(input), t))
 
-end_time = time.time() - start_time
-m, s = divmod(end_time, 60)
-print("The embedding calculation needed {:.0f}min {:.0f}s in total.".format(m, s))
+    end_time = time.time() - start_time
+    m, s = divmod(end_time, 60)
+    print("The embedding calculation needed {:.0f}min {:.0f}s in total.".format(m, s))
+    return input_embedding, times
 
-np.save('times.npy', times)
-np.save('train_input_embedding.npy', train_input_embedding)
+start_time = time.time()
+train_input_embedding, train_times = calculate_and_save_embedding(train_input)
+np.save('../data/train_times.npy', train_times)
+np.save('../data/train_input_embedding.npy', train_input_embedding)
+
+
+start_time = time.time()
+test_input_embedding, test_times = calculate_and_save_embedding(test_input)
+np.save('../data/test_times.npy', test_times)
+np.save('../data/test_input_embedding.npy', test_input_embedding)
