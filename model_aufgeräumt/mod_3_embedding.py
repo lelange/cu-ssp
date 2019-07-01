4b05735a8a1f716b745b1eae144e64174a337ec7
annalena <annalena.alnge>
2019-07-01 18:11:55 +0200

    add vector embedding to mod_3

--- model_aufgeräumt/mod_3_embedding.py
+++ model_aufgeräumt/mod_3_embedding.py
@@ -168,18 +168,18 @@ def build_model():
     input = Input(shape=(X_train.shape[1],))
     profiles_input = Input(shape=(X_aug_train.shape[1], X_aug_train.shape[2]))
     #reshaped = Reshape((None,))(profiles_input)
-    reshaped = Flatten()(profiles_input)
+    #reshaped = Flatten()(profiles_input)
 
     # Defining an embedding layer mapping from the words (n_words) to a vector of len 128
-    #x1 = Embedding(input_dim=n_words, output_dim=250, input_length=None)(input)
-    x1 = Dense(250, activation="relu")(reshaped)
+    x1 = Embedding(input_dim=24, output_dim=250, input_length=None)(input)
+    #x1 = Dense(250, activation="relu")(reshaped)
     print(x1.shape)
     print(input.shape)
-    x1 = concatenate([x1, input])
+    x1 = concatenate([x1, profiles_input])
 
-    #x2 = Embedding(input_dim=n_words, output_dim=125, input_length=None)(input)
-    x2 = Dense(125, activation= "relu")(reshaped)
-    x2 = concatenate([x2, input])
+    x2 = Embedding(input_dim=24, output_dim=125, input_length=None)(input)
+    #x2 = Dense(125, activation= "relu")(reshaped)
+    x2 = concatenate([x2, profiles_input])
     print("x2 shape: ", x2.shape)
 
     x1 = Dense(1200, activation="relu")(x1)
