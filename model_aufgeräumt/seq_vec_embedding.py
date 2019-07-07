0d6ed07a0d0ad944e0b2c21f25d0f613406566ab
annalena <annalena.alnge>
2019-07-07 16:54:41 +0200

    change seq embedding for netsurf data

--- model_aufgeräumt/seq_vec_embedding.py
+++ model_aufgeräumt/seq_vec_embedding.py
@@ -17,6 +17,30 @@ seqvec  = ElmoEmbedder(options,weights,cuda_device=0) # cuda_device=-1 for CPU
 train_input = np.load('../data/netsurfp/train_input.npy')
 test_input = np.load('../data/netsurfp/cb513_input.npy')
 
+def onehot_to_seq(oh_seq, index):
+    s = ''
+    for o in oh_seq:
+        m = np.max(o)
+        if m != 0:
+            i = np.argmax(o)
+            s += index[i]
+        else:
+            break
+    return s
+
+#create sequence representation
+'''
+train_q8 = data['data'][:,:,57:65]
+seq_mask = data['data'][:,:,50]
+
+test = onehot_to_seq(train_q8[0][seq_mask[0]==1], q8_list)
+q8_seq = []
+for i, is_seq in enumerate(seq_mask):
+    seq = onehot_to_seq(train_q8[i][seq_mask[i]>0], q8_list)
+    q8_seq.append(seq)
+
+'''
+
 
 def calculate_and_save_embedding(input):
     # Get embedding for amino acid sequence:
@@ -29,7 +53,7 @@ def calculate_and_save_embedding(input):
         print('----------------------')
         print('Sequence ', (i + 1), '/', len(input))
         print('----------------------')
-        embedding = seqvec.embed_sentence(list(seq))  # List-of-Lists with shape [3,L,1024]
+        embedding = seqvec.embed_sentence(list(onehot_to_seq(seq, list('GHIBESTC') )))  # List-of-Lists with shape [3,L,1024]
 
         # Get 1024-dimensional embedding for per-residue predictions:
         residue_embd = torch.tensor(embedding).sum(dim=0) # Tensor with shape [L,1024]
@@ -50,11 +74,11 @@ def calculate_and_save_embedding(input):
 
 start_time = time.time()
 train_input_embedding, train_times = calculate_and_save_embedding(train_input)
-np.save('../data/train_times_residue.npy', train_times)
-np.save('../data/train_input_embedding_residue.npy', train_input_embedding)
+np.save('../data/train_netsurfp_times_residue.npy', train_times)
+np.save('../data/train_netsurfp_input_embedding_residue_netsurfp.npy', train_input_embedding)
 
 
 start_time = time.time()
 test_input_embedding, test_times = calculate_and_save_embedding(test_input)
-np.save('../data/test_times_residue.npy', test_times)
-np.save('../data/test_input_embedding_residue.npy', test_input_embedding)
+np.save('../data/test_netsurfp_times_residue.npy', test_times)
+np.save('../data/test_netsurfp_input_embedding_residue.npy', test_input_embedding)
