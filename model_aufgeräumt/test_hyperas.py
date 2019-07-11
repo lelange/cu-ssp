351f0b5f1d1725a7900b2615aec482a26e56ba37
annalena <annalena.alnge>
2019-07-11 19:37:48 +0200

    test hyperas

--- model_aufgeräumt/test_hyperas.py
+++ model_aufgeräumt/test_hyperas.py
@@ -1,23 +1,15 @@
-from __future__ import print_function
-import numpy as np
-
-from hyperopt import Trials, STATUS_OK, tpe
-from keras.datasets import mnist
-from keras.layers.core import Dense, Dropout, Activation
-from keras.models import Sequential
-from keras.utils import np_utils
-
 from hyperas import optim
-from hyperas.distributions import choice, uniform
+from hyperas.distributions import quniform, uniform
+from hyperopt import STATUS_OK, tpe, mongoexp
+import keras
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.optimizers import RMSprop
+from keras.datasets import mnist
+import tempfile
 
 
 def data():
-    """
-    Data providing function:
-
-    This function is separated from create_model() so that hyperopt
-    won't reload data for each evaluation run.
-    """
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(60000, 784)
     x_test = x_test.reshape(10000, 784)
@@ -25,65 +17,54 @@ def data():
     x_test = x_test.astype('float32')
     x_train /= 255
     x_test /= 255
-    nb_classes = 10
-    y_train = np_utils.to_categorical(y_train, nb_classes)
-    y_test = np_utils.to_categorical(y_test, nb_classes)
+    num_classes = 10
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
     return x_train, y_train, x_test, y_test
 
 
 def create_model(x_train, y_train, x_test, y_test):
     """
-    Model providing function:
-
-    Create Keras model with double curly brackets dropped-in as needed.
-    Return value has to be a valid python dictionary with two customary keys:
-        - loss: Specify a numeric evaluation metric to be minimized
-        - status: Just use STATUS_OK and see hyperopt documentation if not feasible
-    The last one is optional, though recommended, namely:
-        - model: specify the model just created so that we can later use it again.
+    Create your model...
     """
+    layer_1_size = {{quniform(12, 256, 4)}}
+    l1_dropout = {{uniform(0.001, 0.7)}}
+    params = {
+        'l1_size': layer_1_size,
+        'l1_dropout': l1_dropout
+    }
+    num_classes = 10
     model = Sequential()
-    model.add(Dense(512, input_shape=(784,)))
-    model.add(Activation('relu'))
-    model.add(Dropout({{uniform(0, 1)}}))
-    model.add(Dense({{choice([256, 512, 1024])}}))
-    model.add(Activation({{choice(['relu', 'sigmoid'])}}))
-    model.add(Dropout({{uniform(0, 1)}}))
-
-    # If we choose 'four', add an additional fourth layer
-    if {{choice(['three', 'four'])}} == 'four':
-        model.add(Dense(100))
-
-        # We can also choose between complete sets of layers
-
-        model.add({{choice([Dropout(0.5), Activation('linear')])}})
-        model.add(Activation('relu'))
-
-    model.add(Dense(10))
-    model.add(Activation('softmax'))
-
-    model.compile(loss='categorical_crossentropy', metrics=['accuracy'],
-                  optimizer={{choice(['rmsprop', 'adam', 'sgd'])}})
-
-    result = model.fit(x_train, y_train,
-              batch_size={{choice([64, 128])}},
-              epochs=2,
-              verbose=2,
-              validation_split=0.1)
-    #get the highest validation accuracy of the training epochs
-    validation_acc = np.amax(result.history['val_acc'])
-    print('Best validation acc of epoch:', validation_acc)
-    return {'loss': -validation_acc, 'status': STATUS_OK, 'model': model}
+    model.add(Dense(int(layer_1_size), activation='relu'))
+    model.add(Dropout(l1_dropout))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=RMSprop(),
+                  metrics=['accuracy'])
+    model.fit(x_train, y_train, batch_size=128, epochs=10, validation_data=(x_test, y_test))
+    score, acc = model.evaluate(x_test, y_test, verbose=0)
+    out = {
+        'loss': -acc,
+        'score': score,
+        'status': STATUS_OK,
+        'model_params': params,
+    }
+    # optionally store a dump of your model here so you can get it from the database later
+    temp_name = tempfile.gettempdir()+'/'+next(tempfile._get_candidate_names()) + '.h5'
+    model.save(temp_name)
+    with open(temp_name, 'rb') as infile:
+        model_bytes = infile.read()
+    out['model_serial'] = model_bytes
+    return out
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    trials = mongoexp.MongoTrials('mongo://username:pass@mongodb.host:27017/jobs/jobs', exp_key='mnist_test')
     best_run, best_model = optim.minimize(model=create_model,
                                           data=data,
                                           algo=tpe.suggest,
-                                          max_evals=5,
-                                          trials=Trials())
-    X_train, Y_train, X_test, Y_test = data()
-    print("Evalutation of best performing model:")
-    print(best_model.evaluate(X_test, Y_test))
+                                          max_evals=10,
+                                          trials=trials,
+                                          keep_temp=True)  # this last bit is important
     print("Best performing model chosen hyper-parameters:")
     print(best_run)
