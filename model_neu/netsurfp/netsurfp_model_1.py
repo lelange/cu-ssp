1b4e668aeee0e71c7fa25e8bbb9d83cabadd2ba3
annalena <annalena.alnge>
2019-07-20 23:10:32 +0200

    print model history

--- model_neu/netsurfp/netsurfp_model_1.py
+++ model_neu/netsurfp/netsurfp_model_1.py
@@ -183,7 +183,7 @@ def evaluate_model(model, load_file, test_ind = None):
         names.append(file_test[i])
     return dict(zip(names, test_accs))
 
-def crossValidation(load_file, X_train_aug, y_train, n_folds=1):
+def crossValidation(load_file, X_train_aug, y_train, n_folds=2):
     X_train, X_aug_train = X_train_aug
     # Instantiate the cross validator
     kfold_splits = n_folds
