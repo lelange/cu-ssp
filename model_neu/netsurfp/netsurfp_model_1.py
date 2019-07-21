c9b07fc772597b43f797681b9351a39a81fe8288
annalena <annalena.alnge>
2019-07-21 19:05:59 +0200

    calculate q3 and q8 accuracy, print and save them

--- model_neu/netsurfp/netsurfp_model_1.py
+++ model_neu/netsurfp/netsurfp_model_1.py
@@ -250,13 +250,13 @@ def accuracy2(y_true, y_predicted):
     print("understand metric:")
     y = tf.argmax(y_true, axis =- 1)
     print("y:")
-    tf.print(y)
+    print(y)
     y_ = tf.argmax(y_predicted, axis =- 1)
     mask = tf.greater(y, 0)
     print('mask:')
-    tf.print(mask)
+    print(mask)
     print("tf boolean mask")
-    tf.print(tf.boolean_mask(y, mask))
+    print(tf.boolean_mask(y, mask))
 
     return K.cast(K.equal(tf.boolean_mask(y, mask), tf.boolean_mask(y_, mask)), K.floatx())
 
@@ -278,12 +278,13 @@ def build_and_predict(model, best_weights, save_pred_file, file_test=['cb513_700
         score = model.evaluate(X_test_aug, y_test)
         print("Accuracy from model evaluate: "+str(score[2]))
         #np.save(PRED_DIR+test+save_pred_file, y_test_pred)
-        acc = accuracy2(y_test, y_test_pred)
+
 
         sess = tf.Session()
         with sess.as_default():
-            tf.print("Accuracy2: "+str(acc))
-
+            acc = accuracy2(y_test, y_test_pred)
+            print("Accuracy2: "+str(acc))
+        sess.run()
         print("Saved predictions to "+PRED_DIR+test+save_pred_file+".")
         q3_pred = []
         q8_pred = []
