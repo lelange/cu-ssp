a4627a0de3c74b814e13cb63c3f745a5c70b2a67
annalena <annalena.alnge>
2019-07-22 17:24:18 +0200

    tidy up mod 2

--- model_neu/netsurfp/netsurfp_model_4.py
+++ model_neu/netsurfp/netsurfp_model_4.py
@@ -21,13 +21,26 @@ import time
 import dill as pickle
 
 from utils import *
+from collections import defaultdict
+from datetime import datetime
 
-start_time = time.time()
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 args = parse_arguments(default_epochs=15)
+batch_size = 64
 
-data_root = '../data/netsurfp/'
-load_file = "./model/mod_4-CB513-"+datetime.now().strftime("%Y_%m_%d-%H_%M")+".h5"
+start_time = time.time()
+MODEL_NAME = 'mod_3'
+save_pred_file = "_pred_3.npy"
+
+N_FOLDS = 10 # for cross validation
+MAXLEN_SEQ = 700 # only use sequences to this length and pad to this length, choose from 600, 608, 700
+NB_CLASSES_Q8 = 9 # number Q8 classes, used in final layer for classification (one extra for empty slots)
+NB_CLASSES_Q3 = 3 # number Q3 classes
+NB_AS = 20 # number of amino acids, length of one-hot endoded amino acids vectors
+NB_FEATURES = 30 # feature dimension
 
 normalize = args.normalize
 standardize = args.standardize
@@ -39,18 +52,22 @@ no_input = args.no_input
 optimize = args.optimize
 cross_validate = args.cv
 tv_perc = args.tv_perc
-batch_size = 64
+test_mode = args.test_mode
+predict_only = args.predict
 
-n_tags = 8
-n_words = 20
+if test_mode:
+    N_FOLDS = 2
+    epochs = 2
 
-file_train = 'train'
-file_test = ['cb513', 'ts115', 'casp12']
+data_root = '../data/netsurfp/'
+weights_file = MODEL_NAME+"-CB513-"+datetime.now().strftime("%Y_%m_%d-%H_%M")+".h5"
+load_file = "./model/"+weights_file
+file_scores = "logs/cv_results.txt"
+file_scores_mean = "logs/cv_results_mean.txt"
 
-#load data
-X_train_aug, y_train = get_data(file_train, hmm, normalize, standardize)
+file_train = 'train_' + str(MAXLEN_SEQ)
+file_test = ['cb513_'+ str(MAXLEN_SEQ), 'ts115_'+ str(MAXLEN_SEQ), 'casp12_'+ str(MAXLEN_SEQ)]
 
-time_data = time.time() - start_time
 
 '''
 
@@ -61,13 +78,12 @@ len_seq = 600
 
 def build_model():
     model = None
+    input = Input(shape=(MAXLEN_SEQ, NB_AS,))
     if hmm:
-        input = Input(shape=(X_train_aug[0].shape[1], X_train_aug[0].shape[2],))
-        profiles_input = Input(shape=(X_train_aug[1].shape[1], X_train_aug[1].shape[2],))
+        profiles_input = Input(shape=(MAXLEN_SEQ, NB_FEATURES,))
         x = concatenate([input, profiles_input])
         inp = [input, profiles_input]
     else:
-        input = Input(shape=(X_train_aug.shape[1], X_train_aug.shape[2],))
         x = input
         inp = input
 
@@ -178,7 +194,7 @@ def build_model():
     x = Dropout(0.1)(x)
 
     # Defining the output layer
-    y = TimeDistributed(Dense(n_tags,
+    y = TimeDistributed(Dense(NB_CLASSES_Q8,
                               activation='softmax',
                               use_bias=False,
                               kernel_initializer='glorot_uniform'))(x)
@@ -197,7 +213,7 @@ Fitting and Predicting
 
 '''
 
-def train_model(X_train_aug, y_train, X_val_aug, y_val, epochs = epochs):
+def build_and_train(X_train_aug, y_train, X_val_aug, y_val, epochs = epochs):
     model = build_model()
 
     earlyStopping = EarlyStopping(monitor='val_accuracy', patience=5, verbose=1, mode='max')
@@ -208,7 +224,7 @@ def train_model(X_train_aug, y_train, X_val_aug, y_val, epochs = epochs):
             epochs=epochs, batch_size=batch_size, callbacks=[checkpointer, earlyStopping, reduce_lr], verbose=1, shuffle=True)
 
     # plot accuracy during training
-    return model
+    return model, history
 
 def evaluate_model(model, load_file, test_ind = None):
     if test_ind is None:
@@ -227,17 +243,88 @@ def evaluate_model(model, load_file, test_ind = None):
     return dict(zip(names, test_accs))
 
 
-if cross_validate :
-    cv_scores, model_history = crossValidation(load_file, X_train_aug, y_train)
-    test_acc = np.mean(cv_scores)
-    print('Estimated accuracy %.3f (%.3f)' % (test_acc, np.std(cv_scores)))
+def crossValidation(load_file, X_train_aug, y_train, n_folds=N_FOLDS):
+    X_train, X_aug_train = X_train_aug
+    # Instantiate the cross validator
+    kfold_splits = n_folds
+    kf = KFold(n_splits=kfold_splits, shuffle=True)
+
+    cv_scores = defaultdict(list)
+    model_history = []
+
+    # Loop through the indices the split() method returns
+    for index, (train_indices, val_indices) in enumerate(kf.split(X_train, y_train)):
+        print('\n\n-----------------------')
+        print("Training on fold " + str(index + 1) + "/" + str(kfold_splits) +"...")
+        print('-----------------------\n')
+
+        # Generate batches from indices
+        X_train_fold, X_val_fold = X_train[train_indices], X_train[val_indices]
+        X_aug_train_fold, X_aug_val_fold = X_aug_train[train_indices], X_aug_train[val_indices]
+        y_train_fold, y_val_fold = y_train[train_indices], y_train[val_indices]
+
+        print("Training new iteration on " + str(X_train_fold.shape[0]) + " training samples, " + str(
+            X_val_fold.shape[0]) + " validation samples...")
+
+        model, history = build_and_train([X_train_fold, X_aug_train_fold], y_train_fold,
+                                  [X_val_fold, X_aug_val_fold], y_val_fold)
+
+        print(history.history)
+
+        test_acc = evaluate_model(model, load_file, test_ind = [0, 1, 2])
+
+        cv_scores['val_accuracy'].append(max(history.history['val_accuracy']))
+
+        for k, v in test_acc.items():
+            cv_scores[k].append(v)
+
+        model_history.append(model)
+
+    return cv_scores, model_history
+
+# write best weight models in file and look for model (eg. mod_1) name in weight name
+best_weights = "model/mod_4-CB513-2019_07_22-15_08.h5"
+
+
+
+#--------------------------------- main ---------------------------------
+
+if predict_only:
+    build_and_predict(build_model(), best_weights, save_pred_file, MODEL_NAME, file_test)
+    test_acc = None
+    time_data = time.time() - start_time
+    save_results = False
 else:
-    X_train_aug, y_train, X_val_aug, y_val = train_val_split(hmm, X_train_aug, y_train, tv_perc)
-    model = train_model(X_train_aug, y_train, X_val_aug, y_val, epochs=epochs)
-    test_acc = evaluate_model(model, load_file)
+    # load data
+    X_train_aug, y_train = get_data(file_train, hmm, normalize, standardize)
+
+    if hmm:
+        print("X train shape: ", X_train_aug[0].shape)
+        print("X aug train shape: ", X_train_aug[1].shape)
+    else:
+        print("X train shape: ", X_train_aug.shape)
+    print("y train shape: ", y_train.shape)
+
+    time_data = time.time() - start_time
+    save_results = True
+
+    if cross_validate:
+
+        cv_scores, model_history = crossValidation(load_file, X_train_aug, y_train)
+        test_accs = save_cv(weights_file, cv_scores, file_scores, file_scores_mean, N_FOLDS)
+        test_acc = test_accs[file_test[0] + '_mean']
+
+    else:
+        X_train_aug, y_train, X_val_aug, y_val = train_val_split(hmm, X_train_aug, y_train, tv_perc)
+        #korrigiere name und return
+        model, history = build_and_train(X_train_aug, y_train, X_val_aug, y_val, epochs=epochs)
+        test_acc = evaluate_model(model, load_file)
 
 time_end = time.time() - start_time
 m, s = divmod(time_end, 60)
 print("The program needed {:.0f}s to load the data and {:.0f}min {:.0f}s in total.".format(time_data, m, s))
 
-telegram_me(m, s, sys.argv[0], test_acc, hmm, standardize, normalize, no_input)
+telegram_me(m, s, sys.argv[0], test_acc, hmm, standardize)
+
+if save_results:
+    save_results_to_file(time_end, MODEL_NAME, weights_file, test_acc, hmm, standardize, normalize)
