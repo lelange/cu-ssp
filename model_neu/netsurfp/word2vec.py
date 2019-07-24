978826216a1dbcad32b4bfff433e8d82176df8cd
annalena <annalena.alnge>
2019-07-24 14:42:52 +0200

    build word2vec embedding

--- model_neu/netsurfp/word2vec.py
+++ model_neu/netsurfp/word2vec.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import time
 
 from utils import get_data
+
+start_time = time.time()
 
 data_root = '/nosave/lange/cu-ssp/data/netsurfp/'
 filename= "train_700"
@@ -44,11 +47,16 @@ for i, oh in enumerate(train_input):
     seq = onehot_to_seq(oh, seq_list)
     prim_seq.append(seq)
 
+
 # create overlapping? 3 grams from sequences
 n_grams = seq2ngrams(prim_seq)
 print('n-gram shape:'+str(n_grams.shape))
 print('first n gram:')
 print(n_grams[0])
+
+gram_time = time.time()-start_time
+m, s = divmod(gram_time, 60)
+print("Needed {:.0f}min {:.0f}s to create the 3 grams.".format(m, s))
 
 # replace 3 gramm with 100 dim embedding
 embed_seq = np.zeros((train_input.shape[0], train_input.shape[1]*100))
@@ -56,6 +64,10 @@ embed_seq = np.zeros((train_input.shape[0], train_input.shape[1]*100))
 for i, grams in enumerate(n_grams):
     for j, g in enumerate(grams):
         embed_seq[i,j:j+100]=word_embed[g]
+
+embed_time = time.time() -gram_time
+m, s = divmod(embed_time, 60)
+print("Needed {:.0f}min {:.0f}s to embed the sequences.".format(m, s))
 
 print(embed_seq[0])
 print(embed_seq[-1])
@@ -69,3 +81,6 @@ print("Saved to disk.")
 
 # save to disk
 
+end_time = time.time() -start_time
+m, s = divmod(end_time, 60)
+print("Needed {:.0f}min {:.0f}s in total.".format(m, s))
