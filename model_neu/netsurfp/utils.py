d9ecfbb40a9530c6a4bd5df1026565bd387623cb
annalena <annalena.alnge>
2019-07-30 14:56:22 +0200

    evaluate cb513

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -589,8 +589,8 @@ def build_and_predict(model, best_weights, save_pred_file, model_name, file_test
         print(mask[np.argmin(q3_accs)])
 
         print("all under 50% accuracy:")
-        bads_q3 = np.argwhere(q3_accs<0.5)
-        bads_q8 = np.argwhere(q8_accs < 0.5)
+        bads_q3 = np.argwhere(np.array(q3_accs)<0.5)
+        bads_q8 = np.argwhere(np.array(q8_accs) < 0.5)
         print("Q3: "+str(len(bads_q3)))
         for i in bads_q3:
             print("Accuracy: "+q3_accs[i])
