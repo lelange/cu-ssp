947cd764b92d4d915e3f8f24911f2427000c42b1
annalena <annalena.alnge>
2019-08-01 14:01:24 +0200

    test new metri c mod 3

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -176,13 +176,11 @@ def accuracy(y_true, y_predicted):
 
 
 def cohens_kappa(y_true, y_pred):
-    print('y_true')
-    tf.print(y_true.eval())
-    y_true_classes = tf.argmax(y_true, 1)
-    print('Shape:')
-    print(tf.shape(y_true_classes.eval()))
-    tf.print(y_true_classes)
-    y_pred_classes = tf.argmax(y_pred, 1)
+
+    y_true_classes = tf.argmax(y_true, -1)
+    y_pred_classes = tf.argmax(y_pred, -1)
+    tf.reshape(y_true_classes, (1, None))
+    tf.reshape(y_pred_classes, (1, None))
     return tf.contrib.metrics.cohen_kappa(y_true_classes, y_pred_classes, 9)[1]
 
 def tf_pearson(y_true, y_pred):
