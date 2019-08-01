e2089eebf99d0d936ad87c088be3a9effd51a169
annalena <annalena.alnge>
2019-08-01 13:29:47 +0200

    test new metri c mod 3

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -176,10 +176,10 @@ def accuracy(y_true, y_predicted):
 
 
 def cohens_kappa(y_true, y_pred):
-    y_true_classes = tf.argmax(y_true, axis =- 1)-1
-    y_pred_classes = tf.argmax(y_pred, axis =- 1)-1
+    y_true_classes = tf.argmax(y_true, 1)
+    y_pred_classes = tf.argmax(y_pred, 1)
     mask = tf.greater(y_true_classes, 0)
-    return tf.contrib.metrics.cohen_kappa(y_true_classes, y_pred_classes,8)[1]
+    return tf.contrib.metrics.cohen_kappa(y_true_classes, y_pred_classes, 9)[1]
 
 
 # Convert probabilities to secondary structure
