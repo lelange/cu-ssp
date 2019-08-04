f3d2fff5509bb3f7e567389102175990eadc672d
annalena <annalena.alnge>
2019-08-04 19:24:58 +0200

    build test word2vec

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -555,6 +555,9 @@ def build_and_predict(model, best_weights, save_pred_file, model_name, file_test
     else:
         file_test = ['cb513_full'] ### uncomment later!!
 
+    input_mask = np.load(data_root+'cb513_700_mask.npy')
+    print(len(input_mask))
+
     # save all accuracys from Q8 and Q3 preditions
     f = open(PRED_DIR + "prediction_accuracy.txt", "a+")
     for test in file_test:
