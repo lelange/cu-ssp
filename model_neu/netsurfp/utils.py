8cfc82a008c723954f5e31a52830e1a2c9042802
annalena <annalena.alnge>
2019-08-04 15:06:53 +0200

    compare to normal inputs

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -96,8 +96,9 @@ def get_data(filename, hmm=True, normalize=False, standardize=True, embedding = 
     outputs=[]
 
     if not no_input:
-        input_seq = np.load(data_root + filename + '_input_features.npy')
-        input_seq=standard(input_seq)
+        input_seq = np.load(data_root + filename + '_input.npy')
+        #input_seq = np.load(data_root + filename + '_input_features.npy')
+        #input_seq=standard(input_seq)
         #input_seq_features = np.load(data_root + filename + '_input_features.npy')
         #input_seq = np.load(data_root + filename + '_input.npy')
         #input_seq = np.concatenate((input_seq, input_seq_features), axis=2)
