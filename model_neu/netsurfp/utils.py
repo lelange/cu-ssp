eadff6c7be6fc427a382e8610ee16f5a07e613c7
annalena <annalena.alnge>
2019-08-05 12:29:04 +0200

    test loss as monitor value

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -553,7 +553,7 @@ def build_and_predict(model, best_weights, save_pred_file, model_name, file_test
     if model_name == "mod_2":
         file_test = ['cb513_768']  ### uncomment later!!
     else:
-        file_test = ['cb513_full'] ### uncomment later!!
+        file_test = ['cb513_full', 'casp12_full', 'ts115_full'] ### uncomment later!!
 
     input_mask = np.load(data_root+'cb513_700_seq_range.npy')
 
