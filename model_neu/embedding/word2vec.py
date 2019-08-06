969ae66a3b3873173cb11fd4e3635ee78be3f1f7
annalena <annalena.alnge>
2019-08-06 11:45:58 +0200

    first steps for n gram embedding

--- model_neu/embedding/word2vec.py
+++ model_neu/embedding/word2vec.py
@@ -27,11 +27,9 @@ def seq2ngrams(seqs, n = 3):
         result = []
         for seq in seqs:
             rest = np.mod(int(len(seq)),n)
-            print(rest)
             if rest != 0:
                 seq +=('C'*(n-rest))
                 rest = np.mod(int(len(seq)), n)
-                print(rest)
             result.append([seq[i:i + n] for i in range(int(len(seq)))])
 
         return np.array(result )
