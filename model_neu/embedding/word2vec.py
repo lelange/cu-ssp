a1dee1a82f67d60a7ba5ed60005ff777e8074a37
annalena <annalena.alnge>
2019-08-06 13:47:14 +0200

    first steps for n gram embedding

--- model_neu/embedding/word2vec.py
+++ model_neu/embedding/word2vec.py
@@ -164,7 +164,7 @@ def get_embedding(dataname='netsurfp', mode='train', data=None, n_gram = N_GRAM)
             print('Error!!!')
         #print(item, l.index(word_vectors[item][0]))
         index2embedding.update({item:embedding_matrix[l.index(word_vectors[item][0])]})
-    return index2embedding
+    return w2v
 
 def embed_data(seqs, index2embedding, n_gram=N_GRAM):
 
@@ -173,7 +173,7 @@ def embed_data(seqs, index2embedding, n_gram=N_GRAM):
 
     for i, grams in enumerate(ngram_seq):
         for j, g in enumerate(grams[:700]):
-            embed_seq[i, j, :] = index2embedding[g]
+            embed_seq[i, j, :] = index2embedding.wv[g]
 
     print(embed_seq.shape)
     return embed_seq
