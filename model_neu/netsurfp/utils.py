fc34b96d6455c572e53eff112eb69d01602f3891
annalena <annalena.alnge>
2019-08-09 18:21:30 +0200

    test protvec model

--- model_neu/netsurfp/utils.py
+++ model_neu/netsurfp/utils.py
@@ -15,7 +15,7 @@ import os, pickle
 import random
 import matplotlib.pyplot as plt
 from datetime import datetime
-import gensim.models.keyedvectors as word2vec
+from gensim.models import word2vec
 
 residue_list = list('ACEDGFIHKMLNQPSRTWVYX') + ['NoSeq']
 q8_list      = list('LBEGIHST') + ['NoSeq']
@@ -99,7 +99,7 @@ def get_data(filename, hmm=True, normalize=False, standardize=True, embedding = 
     outputs.append(input_aug)
     if embedding:
         print('Load word2vec model and embed input data...')
-        model = word2vec.KeyedVectors.load(data_root+'embedding/'+'protVec.model')
+        model = word2vec.Word2Vec.load(data_root+'embedding/'+'swissprot-reviewed-protvec.model')
         embed_seq = embed_data(np.load(data_root + filename + '_q9_AA_str.npy'), model=model)
         input_aug = embed_seq
         if hmm: #try with normal input as well! (3 inputs)
